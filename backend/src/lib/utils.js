d81447e8d205f7bb22428bd3de5cc6594b165f7e
gouthameppili <eppiligoutham@gmail.com>
2025-10-19 20:45:31 +0530

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (userId, res) => {
+
+    const { JWT_SECRET } = process.env;
+    if(!JWT_SECRET){
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+
      const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
          expiresIn: '7d',
      });
